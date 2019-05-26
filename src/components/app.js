88a3079e6a258ebedeb3e4256de26f039e2bc1a0
Andrea Pavia <a.pavia95@gmail.com>
2019-05-26 20:52:10 +0200

    login test

--- src/components/app.js
+++ src/components/app.js
@@ -1,18 +1,41 @@
 import React from 'react'
 import { hot } from 'react-hot-loader'
+import SVG from 'react-inlinesvg'
+
+import AuthHelperMethods from '../helpers/Authentication'
+import Avatar from '../assets/avatar.svg'
 
 class App extends React.Component {
+    state = {
+        auth: new AuthHelperMethods()
+    }
+    componentDidMount() {
+        // console.log(this.state.auth.loggedIn())
+
+        // this.state.auth.login('username', 'password')
+        // .then(res => {
+        //     if (res === false) 
+        //         return alert("Sorry those credentials don't exist!");
+        // })
+        // .catch(err => {
+        //     alert(err);
+        // });
+
+        // console.log(this.state.auth.loggedIn())
+    }
     render() {
         return (
             <div className="container">
                 <header className="header">
                     <div className="header__info info">
-                        <img className="info__avatar" src="" alt="avatar"/>
+                        <SVG 
+                            src={Avatar}
+                        />
                         <h5 className="info__heading">Hello!</h5>
                         <span className="info__subtext">under text</span>
                     </div>
                 </header>
-                <section class="content"></section>
+                <section className="content"></section>
             </div>
         )
     }
